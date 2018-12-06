5b072a18989ae1869cee2b9180269d82d0ea3d35
adrienvaucard <adrien.vaucard@gmail.com>
2018-12-06 12:19:14 +0100

    Add home page style w/ dimens, styles etc...

--- app/src/main/java/com/example/adrien/madrental/HomeActivity.java
+++ app/src/main/java/com/example/adrien/madrental/HomeActivity.java
@@ -1,13 +1,24 @@
 package com.example.adrien.madrental;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView appTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //catch ID
+        appTitle = findViewById(R.id.appTitle);
+
+        //Assign custom font to App Title
+        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/BreeSerif-Regular.ttf");
+        //appTitle.setTypeface(font);
     }
 }
