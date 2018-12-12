e30179cfe651ce478625420cb8070aab417094dc
adrienvaucard <adrien.vaucard@gmail.com>
2018-12-12 08:14:39 +0100

    Add MaskEditText library

--- app/src/main/java/com/example/adrien/madrental/HomeActivity.java
+++ app/src/main/java/com/example/adrien/madrental/HomeActivity.java
@@ -43,25 +43,9 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     public void goToSearch(View view) {
-        String regexp = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
-        if (editTextStartReservation.toString().matches(regexp)) {
 
-            editTextStartReservation.setBackgroundResource(R.drawable.edit_text);
-            if (editTextEndReservation.toString().matches(regexp)) {
-
-                editTextEndReservation.setBackgroundResource(R.drawable.edit_text);
-                Intent intent = new Intent(this, SearchesActivity.class);
-                startActivity(intent);
-            }
-            else {
-                editTextEndReservation.setBackgroundResource(R.drawable.edit_text_red);
-            }
-        }
-        else {
-            editTextStartReservation.setBackgroundResource(R.drawable.edit_text_red);
-        }
-
-
+        Intent intent = new Intent(this, SearchesActivity.class);
+        startActivity(intent);
     }
 
 }
